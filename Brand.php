78020027bde29a3be89e209718cce16c40b70000
riusDeath <vuthithuy10197@gmail.com>
2018-07-31 10:04:32 +0700

    update models 02

---
+++ Brand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    protected $tabel = 'brands';
+
+    protected $fillable = [
+    	    'name', 
+    	    'adress', 
+    	    'description', 
+    	    'status',
+    ];
+
+    public function products()
+    {
+    	    return $this->hasMany('App\Models\Product', 'brand_id','id');
+    }
+
+}
