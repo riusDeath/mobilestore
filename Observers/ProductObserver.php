058c231ffb0fcfa1bee199ac28f4b051d78420ad
riusDeath <vuthithuy10197@gmail.com>
2018-08-06 22:43:40 +0700

    init observers

---
+++ Observers/ProductObserver.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models\Observers;
+
+
+use App\Models\Product;
+use App\Models\Log;
+use Illuminate\Support\Facades\Auth;
+
+class ProductObserver 
+{
+   	public function created(Product $product)
+   	{
+   		Log::create([
+            'user_id' => Auth::id(),
+            'action' => 'create',
+            'targetable_id' => $product->id,
+            'targetable_type' => 'product',
+        ]);
+   	}
+
+   	public function updated(Product $product)
+   	{
+        Log::create([
+            'user_id' => Auth::id(),
+            'action' => 'updated',
+            'targetable_id' => $product->id,
+            'targetable_type' => 'product',
+        ]);
+   	}
+
+}
